99bc28620d386e81c6323c8ac3980baad472f15d
BinyamMamo <binyammamo01@gmail.com>
2024-05-12 21:29:12 +0400

    feat(db): add dotenv and mongodb connection
    - Add dotenv package to manage environment variables
    - Create DBClient class to handle MongoDB connection
    - Add methods to get number of users and files
    - Update MongoDB version to 3.7.4

---
+++ utils/db.js
@@ -0,0 +1,47 @@
+const { MongoClient } = require('mongodb');
+require('dotenv').config()
+
+class DBClient {
+  constructor() {
+    this.connected = false;
+
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const db = process.env.DB_DATABASE || 'files_manager';
+    const url = `mongodb://${host}:${port}/${db}`;
+
+    this.client = new MongoClient(url, { useUnifiedTopology: true });
+    this.client.connect().then(() => {
+      this.connected = true;
+    }).catch((err) => {
+      console.log(err.message);
+      this.connected = false;
+    });
+  }
+
+  isAlive() {
+    return this.connected;
+  }
+
+  async nbUsers() {
+    try {
+      const users = this.client.db().collection('users');
+      const count = await users.countDocuments();
+      return count;
+    } catch (err) {
+      throw new Error(`Couldn't display number of 'users': ${err}`)
+    }
+  }
+
+  async nbFiles() {
+    try {
+      const files = this.client.db().collection('files');
+      const coutn = await files.countDocumets();
+    } catch (err) {
+      throw new Error(`Couldn't display number of 'files': ${err}`)
+    }
+  }
+}
+
+const dbClient = new DBClient()
+module.exports = dbClient;
