e865623b42d98eafb4e7fe2b755ac7bbb89a1e02
BinyamMamo <binyammamo01@gmail.com>
2024-05-15 18:15:32 +0400

    refactor(UsersController): convert to ES6 module and use require for imports

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,39 +1,41 @@
-import bcrypt from 'bcrypt';
-import dbClient from '../utils/db';
+const bcrypt = require('bcrypt');
+const dbClient = require('../utils/db');
 
-export default class UsersController {
-  static async postNew(req, res) {
-    try {
-      const { email, password } = req.body;
+const postNew = async (req, res) => {
+  try {
+    const { email, password } = req.body;
 
-      if (!email) {
-        res.status(400).json({ error: 'Missing email' });
-        throw new Error('Missing email');
-      }
-      if (!password) {
-        res.status(400).json({ error: 'Missing password' });
-        throw new Error('Missing password');
-      }
-
-      const users = dbClient.client.db().collection('users');
-      const user = await users.findOne({ email });
-
-      if (user) {
-        res.status(400).json({ error: 'Already exists' });
-        throw new Error('Already exist');
-      }
-
-      const salt = await bcrypt.genSalt(10);
-      const hashedPassword = await bcrypt.hash(password, salt);
-
-      const newUser = await users.insertOne({
-        email,
-        password: hashedPassword,
-      });
-
-      res.status(201).json({ id: newUser.insertedId, email });
-    } catch (err) {
-      console.error(err);
+    if (!email) {
+      res.status(400).json({ error: 'Missing email' });
+      throw new Error('Missing email');
     }
+    if (!password) {
+      res.status(400).json({ error: 'Missing password' });
+      throw new Error('Missing password');
+    }
+
+    const users = dbClient.client.db().collection('users');
+    const user = await users.findOne({ email });
+
+    if (user) {
+      res.status(400).json({ error: 'Already exists' });
+      throw new Error('Already exist');
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = await users.insertOne({
+      email,
+      password: hashedPassword,
+    });
+
+    res.status(201).json({ id: newUser.insertedId, email });
+  } catch (err) {
+    console.error(err);
   }
+}
+
+module.exports = {
+  postNew,
 }
